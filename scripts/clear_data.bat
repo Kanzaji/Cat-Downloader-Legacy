f3585d82cb4027313a38f7a741249aa94ea881f8
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-02-26 20:19:50 +0100

    Remove not required assets/scripts from the repo

--- scripts/clear_data.bat
+++
@@ -1,2 +0,0 @@
-rd /s /q C:\users\%USERNAME%\appdata\local\Kanzaji
-rd /s /q .\mods
--- scripts/test.bat
+++
@@ -1,7 +0,0 @@
-:START
-@echo off
-REM call clear_data.bat
-java -jar ..\.\target\CatDownloader-DEVELOP-jar-with-dependencies.jar test
-pause
-cls
-goto START
