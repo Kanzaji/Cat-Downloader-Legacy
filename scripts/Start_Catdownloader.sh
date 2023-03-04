022d6b8277d197dcfefa33c35286e73e6e0c34b1
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-03-04 21:39:53 +0100

    Bump version to 1.0 and update Scripts

--- scripts/Start_Catdownloader.sh
+++ scripts/Start_Catdownloader.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-java -jar CatDownloader-DEVELOP-jar-with-dependencies.jar
+# This script is used to configure and run CatDownloader!
+# You can add few additional arguments to change the behaviour of the app.
+# Available arguments:
+# -Mode: / -WorkingDirectory: / -ThreadCount: / -DownloadAttempts: / -SizeVerification: / -SumCheckVerification / -Logger
+# Fore more information, check out the github repo of this app! https://github.com/Kanzaji/Cat-Downloader-Legacy
+
+# You can configure the command here:
+java -jar Cat-Downloader-Legacy-1.0.jar
