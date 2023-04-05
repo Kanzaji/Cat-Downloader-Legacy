8d8f718d8584fd64fe9120fe5944e55c35f86478
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-04-05 16:42:57 +0200

    Update Setup_Git_Hooks.bat

--- scripts/Setup_Git_Hooks.bat
+++ scripts/Setup_Git_Hooks.bat
@@ -1,13 +1,14 @@
 @echo off
 
-set arguments = 
-set version = 1.0
+REM You set your arguments here
+set arguments=
 
+set version=1.0
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
 echo java -jar Cat-Downloader-Legacy-%version%.jar %arguments% >> .git/hooks/post-merge
 
-echo Git Hooks have been setted up! Running Cat Downloader...
+echo Git Hooks have been setted up! Running Cat Downloader %version%...
 
 java -jar Cat-Downloader-Legacy-%version%.jar %arguments%
 
