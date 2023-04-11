46248ebcd5fc87617aa5566f242e7ea68690ec7a
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-04-11 09:10:30 +0200

    Update Setup_Git_Hooks.sh

--- scripts/Setup_Git_Hooks.sh
+++ scripts/Setup_Git_Hooks.sh
@@ -1,9 +1,12 @@
-ARGUMENTS=
+#!/bin/sh
+
+ARGUMENTS_SETUP=
+ARGUMENTS_LAUNCH=
 
 VERSION="1.1"
 echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar Cat-Downloader-Legacy-${version}.jar ${arguments}" >> .git/hooks/post-merge
+echo "java -jar Cat-Downloader-Legacy-${version}.jar ${arguments_launch}" >> .git/hooks/post-merge
 
-echo Git Hooks have been setted up! Running Cat Downloader...
+echo Git Hooks have been set up! Running Cat Downloader...
 
-java -jar Cat-Downloader-Legacy-${version}.jar ${arguments}
+java -jar Cat-Downloader-Legacy-${version}.jar ${arguments_setup}
